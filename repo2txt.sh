d71cc05abf7fcf9b5d46104104194f06c7f190d4
thr3a <thr3a@example.com>
2025-01-30 23:22:55 +0900

    add:

--- repo2txt.sh
+++ repo2txt.sh
@@ -1,5 +1,5 @@
 docker run --rm -v ./:/app thr3a/repo2text \
 -d src \
 -e .ts .tsx .css \
--f README.md package.json index.html postcss.config.cjs vite.config.ts \
+-f README.md package.json postcss.config.cjs \
 > repo.md
